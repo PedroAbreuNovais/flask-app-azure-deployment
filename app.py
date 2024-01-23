642fd8793c1d5b1ce6c27402ea6ffd42d1b51ead
PedroAbreuNovais <157099938+PedroAbreuNovais@users.noreply.github.com>
2024-01-23 10:51:43 +0400

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def submit():
     date = "2021-02-04T19:48:17.000Z"
     terminal_id = "7"
     tx_id = "4fc665829b61724781c47042ec17f81456dfee3fc20d7f256818e3ee4f44062"
-    tx_payment = "POS"
+    #tx_payment = "POS"
 
     # API endpoint
     api_url = f"https://api-ad2bfb4d-4efc3229-dku.eu-west-3.app.dataiku.io/public/api/v1/cc_f/sc/run?customer_id={customer_id}&date={date}&terminal_id={terminal_id}&tx_amount={tx_amount}&tx_id={tx_id}&tx_payment={tx_payment}"
