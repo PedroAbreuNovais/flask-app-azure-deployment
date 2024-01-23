d6c504c0048f52ec9833f8af95abcc86f90dd594
PedroAbreuNovais <157099938+PedroAbreuNovais@users.noreply.github.com>
2024-01-23 09:53:22 +0400

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, jsonify
+from flask import Flask, request, jsonify
 import requests
 
 app = Flask(__name__)
@@ -11,12 +11,12 @@ def index():
 def submit():
     customer_id = request.form['customer_id']
     tx_amount = request.form['tx_amount']
+    terminal_id = request.form['terminal_id']
+    tx_payment = request.form['tx_payment']
 
     # Static data
     date = "2021-02-04T19:48:17.000Z"
-    terminal_id = "7"
     tx_id = "4fc665829b61724781c47042ec17f81456dfee3fc20d7f256818e3ee4f44062"
-    tx_payment = "POS"
 
     # API endpoint
     api_url = f"https://api-ad2bfb4d-4efc3229-dku.eu-west-3.app.dataiku.io/public/api/v1/cc_f/sc/run?customer_id={customer_id}&date={date}&terminal_id={terminal_id}&tx_amount={tx_amount}&tx_id={tx_id}&tx_payment={tx_payment}"
