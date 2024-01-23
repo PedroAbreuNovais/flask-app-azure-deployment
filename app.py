c046aa06cd71e558041e1cb43c57b40d8a7d18c2
PedroAbreuNovais <157099938+PedroAbreuNovais@users.noreply.github.com>
2024-01-23 09:32:55 +0400

    Update app.py

--- app.py
+++ app.py
@@ -1,30 +1,28 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, render_template, jsonify
 import requests
 
 app = Flask(__name__)
 
-@app.route('/submit', methods=['POST'])
-def submit_form():
-    # Extracting data from the form
-    customer_id = request.form.get('customer_id')
-    tx_amount = request.form.get('tx_amount')
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-    # Static data (as per your API requirement)
+@app.route('/submit', methods=['POST'])
+def submit():
+    customer_id = request.form['customer_id']
+    tx_amount = request.form['tx_amount']
+
+    # Static data
     date = "2021-02-04T19:48:17.000Z"
     terminal_id = "7"
     tx_id = "4fc665829b61724781c47042ec17f81456dfee3fc20d7f256818e3ee4f44062"
     tx_payment = "POS"
 
-    # Construct the API URL with the data
-    #api_url = f"https://api-ad2bfb4d-4efc3229-dku.eu-west-3.app.dataiku.io/public/api/v1/cc_f/sc/run?customer_id={customer_id}&date={date}&terminal_id={terminal_id}&tx_amount={tx_amount}&tx_id={tx_id}&tx_payment={tx_payment}"
+    # API endpoint
+    api_url = f"https://api-ad2bfb4d-4efc3229-dku.eu-west-3.app.dataiku.io/public/api/v1/cc_f/sc/run?customer_id={customer_id}&date={date}&terminal_id={terminal_id}&tx_amount={tx_amount}&tx_id={tx_id}&tx_payment={tx_payment}"
 
-    # Make a GET request to the external API
-    #try:
-    #    response = requests.get(api_url)
-     #   response.raise_for_status()
-     #   return jsonify(response.json())  # Return the JSON response from the API
-    #except requests.RequestException as e:
-    #    return jsonify({'error': str(e)}), 500
+    response = requests.get(api_url)
+    return jsonify(response.json())
 
 if __name__ == '__main__':
     app.run(debug=True)
