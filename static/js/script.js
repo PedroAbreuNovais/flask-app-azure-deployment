f70ce4bdc88aa81418aed03e3b32432af45121f8
PedroAbreuNovais <157099938+PedroAbreuNovais@users.noreply.github.com>
2024-01-23 09:18:19 +0400

    Update script.js

--- static/js/script.js
+++ static/js/script.js
@@ -1,19 +1,19 @@
-document.getElementById('ccForm').onsubmit = function(event) {
-    event.preventDefault();
+//document.getElementById('ccForm').onsubmit = function(event) {
+ //   event.preventDefault();
 
     // Fetch form data
-    var formData = new FormData(document.getElementById('ccForm'));
+   // var formData = new FormData(document.getElementById('ccForm'));
 
     // Send data to Flask backend
-    fetch('/submit', {
-        method: 'POST',
+    //fetch('/submit', {
+     //   method: 'POST',
         body: formData
+    //})
+    //.then(response => response.json())
+    //.then(data => {
+    //    console.log(data); // Handle the response data
     })
-    .then(response => response.json())
-    .then(data => {
-        console.log(data); // Handle the response data
-    })
-    .catch(error => {
+    //.catch(error => {
         console.error('Error:', error); // Handle errors
-    });
-};
+   // });
+//};
