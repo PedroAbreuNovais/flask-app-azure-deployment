a92e5239df20771308632f7fc46590920ed7e101
PedroAbreuNovais <157099938+PedroAbreuNovais@users.noreply.github.com>
2024-02-02 18:26:47 +0400

    Update app.py

--- app.py
+++ app.py
@@ -4,48 +4,48 @@ from datetime import datetime
 app = Flask(__name__)
 
 # Database configuration from environment variables
-server = os.getenv('ccfdb.database.windows.net')
-database = os.getenv('ccfdb')
-username = os.getenv('adminpedro')
-password = os.getenv('9UAUGKqz=oF;T38wJNKj')
-driver = '{ODBC Driver 17 for SQL Server}'
+#server = os.getenv('ccfdb.database.windows.net')
+#database = os.getenv('ccfdb')
+#username = os.getenv('adminpedro')
+#password = os.getenv('9UAUGKqz=oF;T38wJNKj')
+#driver = '{ODBC Driver 17 for SQL Server}'
 
 
 # SQL commands to create the tables if they don't exist
-create_requests_table = """
-IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='Requests' and xtype='U')
-CREATE TABLE Requests (
-    RequestId INT PRIMARY KEY IDENTITY(1,1),
-    CustomerId NVARCHAR(50),
-    TransactionAmount FLOAT,
-    TerminalId NVARCHAR(50),
-    TransactionPayment NVARCHAR(50),
-    RequestTime DATETIME
-);
-"""
+#create_requests_table = """
+#IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='Requests' and xtype='U')
+#CREATE TABLE Requests (
+#    RequestId INT PRIMARY KEY IDENTITY(1,1),
+#    CustomerId NVARCHAR(50),
+#    TransactionAmount FLOAT,
+#    TerminalId NVARCHAR(50),
+#    TransactionPayment NVARCHAR(50),
+#    RequestTime DATETIME
+#);
+#"""
 
-create_responses_table = """
-IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='Responses' and xtype='U')
-CREATE TABLE Responses (
-    ResponseId INT PRIMARY KEY IDENTITY(1,1),
-    RequestId INT FOREIGN KEY REFERENCES Requests(RequestId),
-    ApiResponse NVARCHAR(MAX),
-    ResponseTime DATETIME
-);
-"""
+#create_responses_table = """
+#IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='Responses' and xtype='U')
+#CREATE TABLE Responses (
+#    ResponseId INT PRIMARY KEY IDENTITY(1,1),
+#    RequestId INT FOREIGN KEY REFERENCES Requests(RequestId),
+#    ApiResponse NVARCHAR(MAX),
+#    ResponseTime DATETIME
+#);
+#"""
 
 # Initialize database connection and create tables if they don't exist
-def init_db():
-    conn = pyodbc.connect(f'DRIVER={driver};SERVER={server};DATABASE={database};UID={username};PWD={password}')
-    cursor = conn.cursor()
-    cursor.execute(create_requests_table)
-    cursor.execute(create_responses_table)
-    conn.commit()
-    cursor.close()
-    conn.close()
+#def init_db():
+#    conn = pyodbc.connect(f'DRIVER={driver};SERVER={server};DATABASE={database};UID={username};PWD={password}')
+#    cursor = conn.cursor()
+ #   cursor.execute(create_requests_table)
+ #   cursor.execute(create_responses_table)
+ #   conn.commit()
+ #   cursor.close()
+  #  conn.close()
 
 # Run the database initialization
-init_db()
+#init_db()
 
 
 @app.route('/')
@@ -69,13 +69,13 @@ def submit():
     #tx_payment = "POS"
 
   # Database connection
-    conn = pyodbc.connect(f'DRIVER={driver};SERVER={server};DATABASE={database};UID={username};PWD={password}')
-    cursor = conn.cursor()
+#    conn = pyodbc.connect(f'DRIVER={driver};SERVER={server};DATABASE={database};UID={username};PWD={password}')
+#    cursor = conn.cursor()
 
     # Insert request data into Requests table
-    cursor.execute("INSERT INTO Requests (CustomerId, TransactionAmount, TerminalId, TransactionPayment, RequestTime) OUTPUT INSERTED.RequestId VALUES (?, ?, ?, ?, GETUTCDATE())", (customer_id, tx_amount, terminal_id, tx_payment))
-    request_id = cursor.fetchone()[0]
-    conn.commit()
+ #   cursor.execute("INSERT INTO Requests (CustomerId, TransactionAmount, TerminalId, TransactionPayment, RequestTime) OUTPUT INSERTED.RequestId VALUES (?, ?, ?, ?, GETUTCDATE())", (customer_id, tx_amount, terminal_id, tx_payment))
+ #   request_id = cursor.fetchone()[0]
+ #   conn.commit()
     
     # API endpoint
     api_url = f"https://api-ad2bfb4d-4efc3229-dku.eu-west-3.app.dataiku.io/public/api/v1/cc_f/sc/run?customer_id={customer_id}&date={date}&terminal_id={terminal_id}&tx_amount={tx_amount}&tx_id={tx_id}&tx_payment={tx_payment}"
@@ -83,11 +83,11 @@ def submit():
     response = requests.get(api_url)
 
    # Insert response data into Responses table
-    cursor.execute("INSERT INTO Responses (RequestId, ApiResponse, ResponseTime) VALUES (?, ?, GETUTCDATE())", (request_id, response.text))
-    conn.commit()
+  #  cursor.execute("INSERT INTO Responses (RequestId, ApiResponse, ResponseTime) VALUES (?, ?, GETUTCDATE())", (request_id, response.text))
+  #  conn.commit()
     # Close the database connection
-    cursor.close()
-    conn.close()
+   # cursor.close()
+   # conn.close()
     return jsonify(response.json())
 
 if __name__ == '__main__':
